5e7e05aeb03391e5066255041fccd8b5a7c8ddb8
Craig Sheen <craig_sheen@hotmail.com>
2016-01-20 20:42:13 +1100

    Add some more terrible commits

---
+++ app/models/foobar.rb
@@ -0,0 +1,7 @@
+class Foobar < ActiveRecord::Base
+  def zzzzzzz
+    a = 1
+    b = 2
+    a + b
+  end
+end
--- app/models/something.rb
+++ app/models/something.rb
@@ -2,4 +2,8 @@ class Something < ActiveRecord::Base
   def a
     1+1
   end
+
+  def b
+
+  end
 end
